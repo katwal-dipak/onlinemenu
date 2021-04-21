7b4221574d3b6dfce0cf62e806a53c6aee248851
Dipak Katwal <katwal.dipak@hotmail.com>
2021-04-21 07:52:05 +0545

    minor chnages

--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -2,25 +2,18 @@ import React from 'react';
 
 import {View, Text, FlatList, SafeAreaView, RefreshControl} from 'react-native';
 
-import {PostsData} from './PostsData';
 import {MenuCard} from '../../components';
 
 const Home = ({}) => {
   const keyExtractor = (item, index) => index.toString();
 
   const RenderItem = ({item}) => {
-    return <MenuCard imageURL={item.imageURL} />;
+    return <MenuCard imageURL={''} />;
   };
 
   return (
-    <View style={{flex: 1, backgroundColor: '#FFFFFF', alignItems: 'center'}}>
-      <FlatList
-        style={{flex: 1, marginTop: 10}}
-        showsVerticalScrollIndicator={false}
-        data={PostsData}
-        renderItem={RenderItem}
-        keyExtractor={keyExtractor}
-      />
+    <View style={{flex: 1, backgroundColor: '#FFFFFF'}}>
+      <MenuCard />
     </View>
   );
 };
