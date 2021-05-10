b5a61f06ab6a80728ebf7c8a9817d87c5bb6c1bf
Dipak Katwal <katwal.dipak@hotmail.com>
2021-05-10 22:52:26 +0545

    remove redundant codes

--- src/screens/Menu/MenuItems/index.js
+++ src/screens/Menu/MenuItems/index.js
@@ -85,33 +85,3 @@ const MenuItems = ({navigation, route}) => {
 };
 
 export default MenuItems;
-
-const data = [
-  {
-    id: 0,
-    title: 'AFFOGATO',
-    description:
-      'Espresso poured on a vanilla ice cream. Served in a cappuccino cup',
-    active: true,
-    price: '$4.20',
-    data: [1, 2, 3, 4, 5, 6],
-  },
-  {
-    id: 1,
-    title: 'CAFFÈ MOCHA',
-    description:
-      'A caffè latte with chocolate and whipped cream, made by pouring about 2 cl of chocolate sauce into the glass, followed by an espresso shot and steamed milk',
-    active: false,
-    price: '$1.00',
-    data: [1, 2, 3],
-  },
-  {
-    id: 2,
-    title: 'AMERICANO (or ESPRESSO AMERICANO)',
-    description:
-      'Espresso with added hot water (100–150 ml). Often served in a cappuccino cup. (The espresso is added into the hot water rather than all the water being flowed through the coffee that would lead to over extraction.)',
-    active: true,
-    price: '$10.00',
-    data: [1, 2, 3, 4, 5],
-  },
-];
